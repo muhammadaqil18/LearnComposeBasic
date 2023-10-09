9078971ea28de84d309eb8ea7ed53930b855e232
muhammadaqil18 <aqil.pratama@koinworks.com>
2023-10-09 22:51:54 +0700

    add new resources

--- app/src/main/java/com/example/cobabaru/ui/latihan/MainActivity.kt
+++ app/src/main/java/com/example/cobabaru/ui/latihan/MainActivity.kt
@@ -6,7 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.cobabaru.ui.model.Affirmation
 import com.example.cobabaru.ui.theme.CobaBaruTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,9 +22,27 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    AffirmationApp()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun AffirmationApp() {
+    
+}
+
+@Composable
+fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
+    
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun AffirmationAppPreview() {
+    CobaBaruTheme {
+        AffirmationApp()
     }
 }
