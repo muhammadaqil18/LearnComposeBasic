83ebf90466e61360f7a6fa28c4988506c06092d1
muhammadaqil18 <aqil.pratama@koinworks.com>
2023-10-28 13:58:46 +0700

    create topic dataclass

--- app/src/main/java/com/example/cobabaru/ui/model/Topic.kt
+++ app/src/main/java/com/example/cobabaru/ui/model/Topic.kt
@@ -3,7 +3,8 @@ package com.example.cobabaru.ui.model
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
-data class Affirmation(
-    @StringRes val stringResourceId: Int,
-    @DrawableRes val imageResourceId: Int
+data class Topic(
+    @StringRes val jobTitle: Int,
+    val numberOfCourse: Int,
+    @DrawableRes val jobImage: Int
 )
