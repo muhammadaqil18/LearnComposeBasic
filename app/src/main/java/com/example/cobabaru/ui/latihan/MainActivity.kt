906269f1307b9aad909b1a13ceda99dea258c247
muhammadaqil18 <aqil.pratama@koinworks.com>
2023-10-28 12:45:09 +0700

    set scrollable list

--- app/src/main/java/com/example/cobabaru/ui/latihan/MainActivity.kt
+++ app/src/main/java/com/example/cobabaru/ui/latihan/MainActivity.kt
@@ -3,12 +3,28 @@ package com.example.cobabaru.ui.latihan
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.cobabaru.R
+import com.example.cobabaru.ui.data.DataSource
 import com.example.cobabaru.ui.model.Affirmation
 import com.example.cobabaru.ui.theme.CobaBaruTheme
 
@@ -31,18 +47,54 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AffirmationApp() {
-    
+    AffirmationList(affirmationList = DataSource().loadAffirmations())
+}
+
+@Composable
+fun AffirmationList(affirmationList: List<Affirmation>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(affirmationList) { affirmation ->
+            AffirmationCard(
+                affirmation = affirmation,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
 }
 
 @Composable
 fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
-    
+    Card(modifier = modifier) {
+        Column {
+            Image(
+                painter = painterResource(id = affirmation.imageResourceId),
+                contentDescription = stringResource(
+                    id = affirmation.stringResourceId
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(190.dp),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = LocalContext.current.getString(affirmation.stringResourceId),
+                modifier = Modifier
+                    .padding(16.dp),
+                style = MaterialTheme.typography.headlineSmall
+            )
+        }
+    }
 }
 
 @Preview(showSystemUi = true)
 @Composable
 fun AffirmationAppPreview() {
     CobaBaruTheme {
-        AffirmationApp()
+        AffirmationCard(
+            affirmation = Affirmation(
+                stringResourceId = R.string.affirmation1,
+                imageResourceId = R.drawable.image1
+            )
+        )
     }
 }
